b60495c946a1a6fbb3d63c70ac0b483ddf788765
Arunachalam-D <duraipandi1999@gmail.com>
2023-08-23 16:58:24 +0530

    Small change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import  Navbar  from './shared/Navbar';
+import IntroCard from './pages/IntroCard';
 function App() {
   return (
     <div className="App">
      <Navbar/>
+     <IntroCard/>
     </div>
   );
 }
