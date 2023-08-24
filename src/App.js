7f4ff000d64129fba425cdd5c011fe78590b75e8
Jaswanth Kumar <jgjaswanthjk6@gmail.com>
2023-08-24 18:37:12 +0530

    login and footer

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import LoginSignin from './shared/Login';
 import  Navbar  from './shared/Navbar';
 import IntroCard from './pages/IntroCard';
+import Footer from './shared/Footer';
 function App() {
   return (
     <div className="App">
-     <Navbar/>
+     {/* <Navbar/> */}
 
+      {/* <Footer/> */}
      <LoginSignin/>
 
-     <IntroCard/>
+     {/* <IntroCard/> */}
 
     </div>
   );
