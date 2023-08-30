33690ed7de0b0a205a615347623f65bc2ff9f9d5
Arunachalam-D <duraipandi1999@gmail.com>
2023-08-30 18:53:06 +0530

    Lessor details added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Login from '../src/auth/Login'
 import IntroPage from './pages/index'
-
 import FurnitureType from './shared/Furniture/index';
 import FitnessType from './shared/Fitness/index';
-
 import ElectronicType from './shared/ElectonicsType/index'
 import Appliances from './shared/Appliances/index'
 import VehicleType from './shared/Vehicles/index';
-
+import Lessor from './pages/Lessor/index'
 
 function App() {
   return (
@@ -20,16 +18,15 @@ function App() {
           <Route path='/' element={<IntroPage/>}/>             
           <Route path='/login' element={<Login/>}/>
           <Route path='/furniture' element={<FurnitureType/>} /> 
-
           <Route path='/electronic' element={<ElectronicType/>} /> 
           <Route path='/fitness' element={<FitnessType/>} /> 
           <Route path='/vehicle' element={<VehicleType/>} />
-          <Route path='/appliances' element={<Appliances/>} />      
-
+          <Route path='/appliances' element={<Appliances/>} /> 
+          <Route path='/lessor' element={<Lessor/>} />     
         </Routes>
      </BrowserRouter>
   
-
+    
     </div>
   );
 }
