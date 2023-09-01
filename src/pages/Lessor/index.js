76d1f731999857514d1137f6222fb95fa9a93382
Arunachalam-D <duraipandi1999@gmail.com>
2023-09-01 18:04:46 +0530

    worked on Filter and added smart devices

--- src/pages/Lessor/index.js
+++ src/pages/Lessor/index.js
@@ -31,7 +31,7 @@ export default function Example() {
           </div>
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-4">
-              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+              <label className="block text-sm font-medium leading-6 text-gray-900">
                 Product name / brand
               </label>
               <div className="mt-2">
@@ -40,7 +40,7 @@ export default function Example() {
                   <input
                     type="text"
                     name="username"
-                    id="username"
+                    id="productname"
                     autoComplete="username"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     placeholder='eg : 32" LED TV /samsung'
@@ -68,7 +68,7 @@ export default function Example() {
            
 
             <div className="col-span-full">
-              <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
+              <label className="block text-sm font-medium leading-6 text-gray-900">
                 Product photo
               </label>
               <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
@@ -76,7 +76,7 @@ export default function Example() {
                  
                   <div className="mt-4 flex text-sm leading-6 text-gray-600">
                     <label
-                      htmlFor="file-upload"
+
                       className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-0 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
                     >
                       <span className="border-none">Upload atleast 3 photos</span>
@@ -146,7 +146,7 @@ export default function Example() {
               </label>
               <div className="mt-2">
                 <select
-                  id="country"
+                  id="newcity"
                   name="country"
                   autoComplete="country-name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-0 sm:max-w-xs sm:text-sm sm:leading-6"
@@ -195,7 +195,7 @@ export default function Example() {
               </label>
               <div className="mt-2">
                 <input
-                  id="email"
+                  id="mobile"
                   name="email"
                   type="number"
                   autoComplete="email"
@@ -211,7 +211,7 @@ export default function Example() {
               </label>
               <div className="mt-2">
                 <input
-                  id="email"
+                  id="cost"
                   name="email"
                   type="number"
                   autoComplete="email"
